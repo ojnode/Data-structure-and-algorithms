07fb0bb198cfe0ab667681a64c72c6bca3b1d9b3
ojnode <ojinnakabu95@gmail.com>
2024-10-31 13:19:39 +0000

    implement bubble sort

--- src/searchingSorting.java
+++ src/searchingSorting.java
@@ -40,9 +40,30 @@ public class searchingSorting {
         }
         return sorted;
     }
+
+    public static String bubbleSort(int[] arr) {
+        boolean swap = true;
+        int adjecentElement;
+        int nextIndex;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < (arr.length - 1); i++) {
+                nextIndex = i + 1;
+                if (arr[i] > arr[nextIndex]) {
+                    adjecentElement = arr[nextIndex];
+                    arr[nextIndex] = arr[i];
+                    arr[i] = adjecentElement;
+                    swap = true;
+                }
+            }
+        }
+        return Arrays.toString(arr);
+    }
     public static void main(String[] args) {
-        ArrayList<Integer> unsorted= new ArrayList<>(Arrays.asList(9, 5, 3, 2, 1, 7, 6, 8, 1, 0, 15, 2));
+        // ArrayList<Integer> unsorted= new ArrayList<>(Arrays.asList(9, 5, 8, 1, 4,10));
         // System.out.println(selectionSort(unsorted));
-        System.out.println(insertionSort(unsorted));
+        // System.out.println(insertionSort(unsorted));
+        int[] unsorted = {5, 1, 7, 3, 8 ,9 , 1, 10, 0};
+        System.out.println(bubbleSort(unsorted));
     }
 }
