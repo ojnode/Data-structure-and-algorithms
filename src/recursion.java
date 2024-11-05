687935355346daf082de700c7074a3ace54ec5eb
ojnode <ojinnakabu95@gmail.com>
2024-11-05 16:35:08 +0000

    sumOfnnumbers recursion

---
+++ src/recursion.java
@@ -0,0 +1,26 @@
+public class recursion {
+
+    public static int sumOfnNumbers(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        else 
+        {
+            return n + sumOfnNumbers(n-1);
+        }
+    }
+
+    public static void displayNumbers(int x, int y) {
+        int nextInteger = x + 1;
+        if (x < y && nextInteger < y) {
+            System.out.print(nextInteger + " ");
+            displayNumbers(nextInteger, y);
+
+        }
+  
+    }
+    public static void main() {
+        // System.out.println(sumOfnNumbers(12));
+        displayNumbers(3, 8);
+    }
+}
